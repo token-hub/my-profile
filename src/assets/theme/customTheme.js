ff1c1ac8894acb89e21bf9ab132c7fe87ee8b8ea
token-hub <63064941+token-hub@users.noreply.github.com>
2021-10-03 19:35:21 +0800

    Created a custom Theme

---
+++ src/assets/theme/customTheme.js
@@ -0,0 +1,41 @@
+import { createTheme } from '@mui/material/styles';
+
+const theme = createTheme({
+  typography : {
+    fontFamily: 
+      [
+        'Nunito Sans',
+        'Roboto',
+      ].join(','),
+  },
+  palette: {
+    primary: {
+      light: '#757ce8',
+      main: '#3f50b5',
+      dark: '#002884',
+      contrastText: '#fff',
+    },
+    secondary: {
+      light: '#ff7961',
+      main: '#f44336',
+      dark: '#ba000d',
+      contrastText: '#000',
+    },
+    tertiary: {
+      main: '#A9A9A9',
+      dark: '#918e8e',
+    }
+  }
+});
+
+theme.typography.h3 = {
+  fontSize: '3rem',
+  [theme.breakpoints.down('md')]: {
+    fontSize: '2.4rem',
+  },
+  [theme.breakpoints.down('sm')]: {
+    fontSize: '1.5rem',
+  },
+};
+
+export default theme;
