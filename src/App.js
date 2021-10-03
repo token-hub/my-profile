a2264032cc21e2af4400e709ce5fd1fea6422e5b
token-hub <63064941+token-hub@users.noreply.github.com>
2021-10-03 19:44:16 +0800

    Initial Home page draft finished

--- src/App.js
+++ src/App.js
@@ -3,15 +3,16 @@ import Home from './pages/Home';
 import Box from '@mui/system/Box';
 import Appbar from './components/organisms/Appbar';
 import Base from './components/templates/Base';
-
+import Contact from './components/organisms/Contact';
 
 const App = () => {
   return (
-    <Box sx={{height: '100vh', fontFamily: 'Nunito Sans', position: 'relative' }}>
+    <Box sx={{height: 'auto', fontFamily: 'Nunito Sans', position: 'relative'}}>
       <Appbar/>
       <Base>
         <Home />
       </Base>
+      <Contact />
     </Box>
   )
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import MyAvatarAndGreetings from '../components/molecules/MyAvatarAndGreetings';
+import HorizontalLine from '../components/atoms/HorizontalLine';
+import About from '../components/organisms/About';
+import Projects from '../components/organisms/Projects';
 
 const Home = () => {
     return (
        <>
             <MyAvatarAndGreetings />
+            <HorizontalLine />
+            <About />
+            <HorizontalLine />
+            <Projects />
+            <HorizontalLine />
        </>
     )
 }
