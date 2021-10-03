c78d417d9172cb09c29e941786c419428f74a360
token-hub <63064941+token-hub@users.noreply.github.com>
2021-10-03 19:43:50 +0800

    Added ThemeProvider at the index page

--- src/index.js
+++ src/index.js
@@ -2,10 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import theme from './assets/theme/customTheme';
+import {ThemeProvider} from '@mui/material/styles';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
