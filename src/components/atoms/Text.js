0505b037b0de9ee1f2b96b9ad331226706522fa9
token-hub <63064941+token-hub@users.noreply.github.com>
2021-10-03 19:36:45 +0800

    Added padding right option for text component

--- src/components/atoms/Text.js
+++ src/components/atoms/Text.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import Typography from '@mui/material/Typography';
 
-const Text = ({ children, variant = 'h5', textAlign = 'left' }) => {
+const Text = ({ children, variant = 'h5', textAlign = 'left', pr = 5 }) => {
 
     return (
         <>
-            <Typography variant={variant} sx={{ pr: 5, py: 2, textAlign: textAlign, lineHeight: 1.4 }}  >
+            <Typography variant={variant} sx={{pr, py: 2, textAlign: textAlign, lineHeight: 1.4 }}  >
              { children }
             </Typography>
         </>
