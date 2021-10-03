e64a3d7cba84002a855c34fffb16b03b16d5cfca
token-hub <63064941+token-hub@users.noreply.github.com>
2021-10-03 19:42:55 +0800

    Create initial Project content

---
+++ src/components/organisms/Projects.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Heading from '../atoms/Heading';
+import Text from '../atoms/Text';
+
+const Projects = () => {
+    return (
+        <>
+            <Heading> PROJECTS </Heading>
+            <Text textAlign='justify'> Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum. </Text>
+            <Text textAlign='justify'> Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum. </Text>
+        </>
+    )
+}
+
+export default Projects
