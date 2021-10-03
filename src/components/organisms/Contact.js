fbb5fc9ef3a579dea145c589124bb9129775ed8c
token-hub <63064941+token-hub@users.noreply.github.com>
2021-10-03 19:39:46 +0800

    Created Contact draft

---
+++ src/components/organisms/Contact.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import Box from '@mui/material/Box';
+import Link from '@mui/material/Link';
+import ContactLink from '../atoms/ContactLink';
+
+const Contact = () => {
+    return (
+       <Box sx={{ display: 'flex', justifyContent: 'center', fontSize: 20, pb: 15}}>
+          <Box sx={{ display: 'flex', justifyContent: 'space-between', width: 300 }}>
+            <ContactLink color="tertiary.dark"> Facebook </ContactLink>
+            <ContactLink color="tertiary.dark"> Email </ContactLink>
+            <ContactLink color="tertiary.dark"> Resume </ContactLink>
+          </Box>
+       </Box>
+    )
+}
+
+export default Contact
